5b6f704f9bae08b494e14984d5409155a2caa17d
magicHat-github <3306685704@qq.com>
2019-07-19 14:11:30 +0800

    新建分支develop

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
 	public static void main(String[] args){
-		System.out.print("hello world! My Git");
+		System.out.println("hello world! My Git");
+		System.out.println("test Git!!");
 	}
 
 }
